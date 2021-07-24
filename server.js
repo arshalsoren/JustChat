afe053352fb45d7440596a141b782c6d0de482ac
arshalsoren <arshalsoren@gmail.com>
2021-07-24 16:02:49 +0530

    update: room functionality

--- server.js
+++ server.js
@@ -2,34 +2,62 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+const botName = 'administrator';
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // When client connects
 io.on('connection', socket => {
-    // 1. Message only for the client
-    // Welcome current user
-    socket.emit('message', 'Welcome to JustChat!');
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
 
-    // 2. Brodcast message for everyone except the new client
-    // When a user connects
-    socket.broadcast.emit('message', 'User has joined the chat');
+        // 1. Message only for the client
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to JustChat!'));
 
-    // When a user disconnects
-    socket.on('disonnect', () => {
-        // 3. Message for every client
-        io.emit('message', 'User has left the chat');
+        // 2. Brodcast message for everyone except the new client
+        // When a user connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                'message',
+                formatMessage(botName, `${user.username} has joined the chat`)
+            );
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', (msgText) => {
-        io.emit('message', msgText);
-    })
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room)
+            .emit(
+                'message',
+                formatMessage(user.username, msgText)
+            );
+    });
+
+    // When a user disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+        if (user) {
+
+            // 3. Message for every client
+            io.to(user.room)
+                .emit(
+                    'message',
+                    formatMessage(botName, `${user.username} has left the chat`)
+                );
+        }
+    });
 });
 
 // Server Route
